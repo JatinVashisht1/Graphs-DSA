d6c2ef6a6913ab2535241555ba1c5699e415bae6
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-08-23 14:01:20 +0530

    used vector of vector instead of array of vector

--- 1_graph_representation.cpp
+++ 1_graph_representation.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+
+// using array of vector
 void addEdge(vector<int> adj[], int source, int destination){
     adj[source].push_back(destination);
     adj[destination].push_back(source);
@@ -17,11 +19,34 @@ void printGraph(vector<int> adj[], int size){
     }
 }
 
+// using vector of vector
+void addEdgeVec(vector<vector<int>> &adj, int source, int destination){
+    adj[source].push_back(destination);
+    adj[destination].push_back(source);
+}
+
+void printGraphVec(vector<vector<int>> &adj){
+    for(int i = 0; i<adj.size(); i++){
+        cout<<i<<"-> ";
+        for(auto it: adj[i]){
+            cout<<it<<" ";
+        }
+        cout<<'\n';
+    }
+}
+
+
 int main(){
     vector<int> adj[5];
     addEdge(adj, 0, 3);
     addEdge(adj, 2, 1);
     addEdge(adj, 3, 2);
     printGraph(adj, 5);
+    cout<<"--------\n";
+    vector<vector<int>> adjVec(5);
+    addEdgeVec(adjVec, 0, 3);
+    addEdgeVec(adjVec, 2, 1);
+    addEdgeVec(adjVec, 3, 2);
+    printGraphVec(adjVec);
     return 0;
 }
