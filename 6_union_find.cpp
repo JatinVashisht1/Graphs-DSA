5cb2fc0e70c63f2ed0a8fe7c1da1ef9591e0ec66
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-09-06 10:14:48 +0530

    union find with optimization: path compression and union by size

--- 6_union_find.cpp
+++ 6_union_find.cpp
@@ -4,23 +4,36 @@ using namespace std;
 const int N = 1e5 + 10;
 
 int parent[N];
+// array to track size of each tree
+int size[N];
 
 void make(int i){
     parent[i] = i;
+    size[i] = 1;
 }
 
 int find(int i){
     if(parent[i] == i){
         return i;
     }
-
-    return find(parent[i]);
+    
+    // OPTIMIZATION: make parent of leaf nodes parent of their parent only
+    // This will prevent tree from becoming skewed
+    // This is called PATH COMPRESSION
+    return parent[i] = find(parent[i]);
 }
 
 int Union(int x, int y){
     int parent_x = find(x);
     int parent_y = find(y);
-    parent[parent_x] = parent_y;
+    if(parent_x != parent_y){
+        if(parent_x != parent_y)
+            swap(parent_x, parent_y);
+        parent[parent_x] = parent_y;
+        // OPTIMIZATION: this is called union find by SIZE
+        // this operation will prevent tree from getting skewed because we are merging node with tree of smaller size
+        size[parent_x] += parent_y;
+    }
 }
 
 int main(){
@@ -33,7 +46,7 @@ int main(){
     // }
 
     Union(0, 1);
-    Union(1, 3);
+    Union(2, 3);
 
     for(int i = 0; i<n; i++){
         cout<<"parent of "<<i<<" is "<<find(i)<<'\n';
